ad8e98ed943f50744c353792d55c2ff5f95744e2
张翔 <zhangxiang@kingwant.com>
2019-10-31 14:57:22 +0800

    update

--- globalFunctionES5.js
+++ globalFunctionES5.js
@@ -1,30 +1,30 @@
-export default {
+var tool = (function () {
     /**
-     * @param {option} 需要深拷贝的对象 
-     */
-    deepCopy(option) {
+      * @param {option} 需要深拷贝的对象 
+      */
+    function deepCopy(option) {
         return JSON.parse(JSON.stringify(option));
-    },
+    }
     /**
+     * 
      * @method {toTree} 将有父子关系的数据整理成树结构
      * @param {origin} 需要处理的源数据
      * @param {rootId} tree根节点id 
      */
-    toTree(origin, rootId) {
-        // let cloneOrigin = JSON.parse(JSON.stringify(origin));
+    function toTree(origin, rootId) {
         let cloneOrigin = this.deepCopy(origin);
         return cloneOrigin.filter(father => {
             let childArr = cloneOrigin.filter(child => child.pid == father.id);
             childArr.length > 0 ? father.children = childArr : '';
             return father.pid == rootId
         })
-    },
+    }
     /**
      * 
-     * @param {treeData} Array 
+     * @param {treeData}  Array
      * @param {treeKey} Array 每一个树节点的属性
      */
-    expandTree(treeData, treeKey) {
+    function expandTree(treeData, treeKey) {
         let stack = this.deepCopy(treeData),
             data = [];
         while (stack.length != 0) {
@@ -44,4 +44,14 @@ export default {
         }
         return data;
     }
-}
+
+
+
+    return {
+        deepCopy: deepCopy,
+        toTree: toTree,
+        expandTree: expandTree
+    }
+
+
+})()
